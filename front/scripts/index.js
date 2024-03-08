23bc21ba1897b7b5910fa4ffcfd4f66b8ebbfcdc
AljureS <saidsimon2@gmail.com>
2024-03-07 19:45:04 -0500

    Como se have un bundle

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,27 +1,6 @@
-const seccionPeliculas = document.getElementById("allMoviesCards")
+const renderFilms = require("./renderCards")
 
-function renderFilms(pelicula) {
-    const peliculaElemento = document.createElement('article')
-    const tarjeta = document.createElement("div")
-
-    tarjeta.classList.add("card__content")
-    peliculaElemento.classList.add("responsiveBox")
-
-    tarjeta.innerHTML =`
-        <h3 id ="movieTitle">${pelicula.title}</h3>
-        <p class="card__title">${pelicula.year}</p>
-        <p class="card__title">${pelicula.director}</p>
-        <p class="card__title">${pelicula.genre.join(', ')}</p>
-        <p class="card__title">${pelicula.rate}</p>
-        <img class="imageMovies" src="${pelicula.poster}">
-    `
-    seccionPeliculas.appendChild(peliculaElemento)
-    peliculaElemento.appendChild(tarjeta)
-}
-
-const allmovies = data => data.map(renderFilms)
-
-$.get('https://students-api.2.us-1.fl0.io/movies', allmovies)
+$.get('https://students-api.2.us-1.fl0.io/movies', data => data.map(renderFilms))
 
 
 

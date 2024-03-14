1c9711e6ca1ca80bc5fbd4aecf41f1ba278eab83
AljureS <saidsimon2@gmail.com>
2024-03-14 15:32:34 -0500

    Credito extra en la tarea clase 8

--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -10,7 +10,6 @@ module.exports ={
                 error: error.message
             })
         }
-
         
     }
 }
--- back/src/services/movieService.js
+++ back/src/services/movieService.js
@@ -1,12 +1,27 @@
 const axios = require("axios")
+class Movie {
+    constructor({ title, year, director, duration, genre, rate, poster }){
+        if (!title || !poster || !director) {
+            throw new Error('Missing required properties: title, poster or director')
+        }
+        this.title= title,
+        this.poster = poster,
+        this.year = year,
+        this.director = director,
+        this.duration = duration,
+        this.genre = genre,
+        this.rate = rate
+    }
+}
 
 module.exports ={
     getServiceMovies: async () =>{
         try {
-            const movies = await axios ('https://students-api.up.railway.app/movies')
-            return movies.data
+            const movies = await axios('https://students-api.up.railway.app/movies')
+            const movieConstructor = movies.data.map((peli) => new Movie(peli))
+            return movieConstructor
         } catch (error) {
-            console.log('no te funciono wey, algo paso');
+            console.error('no te funciono wey, algo paso');
         }
     }
 }
